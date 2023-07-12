928ba71f5e06341689f491bf127f1fe5871b4b94
Celso Cardenas <cardenascelso9@gmail.com>
2023-07-11 22:17:46 -0600

    dia 3 | lo que dice el proyecto va aquí xD, proyecto general, nada complejo

--- third_day/theory.py
+++ third_day/theory.py
@@ -17,3 +17,48 @@ print(f"Extrayendo por indice {result}")
 # Esto extraerá la primer palabra de la cadena total
 fragment = my_text[0:4]
 print(f"Extrayendo por rango {fragment}")
+
+# ---------------------------------------------------------------------------
+# Diccionarios
+# Un objeto en JS
+# entidad clave-valor, se usan para acceder a información sin conocer su posición
+dictionary = {
+    'c1': 'primer valor',
+    'c2': 'segundo valor'
+}
+
+print(dictionary)
+
+# Para acceder, es necesario indicar la posición/atributo por medio del nombre 
+# pero con sintaxis de un arreglo/lista
+print(dictionary['c1'])
+
+# Ejemplo de consulta
+dic = {'c1': ['a','b','c'], 'c2': ['d', 'e', 'f']}
+print(dic['c2'][1].upper())
+
+# ---------------------------------------------------------------------------
+# Tuplas
+# Las tuplas son lo mismo que una lista... pero inmutables
+# su sintaxis esigual que una lista, pero entre paréntesis
+
+# Para convertir una tupla a una lista para modificarla...
+my_tuple = 1,2,3,4,5
+new_tuple = list(my_tuple)
+
+# ---------------------------------------------------------------------------
+# Sets
+# Los sets son lo mismo que una tupla... pero sólo admiten elementos únicos
+
+# Para declararlos es necesario usar llaves o enviar una lista a una función set()
+my_set = {1,2,3,4,5}
+
+# Dentro de un set, no se pueden almacenar ni diccionarios ni listas, unicamente valores únicos o tuplas
+# Tiene operadores como "len" o consultas de existencia (something in set)
+
+# para fusionar sets se usa la función union()
+my_new_set = {6,7,8,9}
+last_set = my_set.union(my_new_set)
+
+# Cuenta con la función "add" y "remove" para agregar o eliminar elementos
+# en caso que se mencione uno ya existente, simplemente se ignora
